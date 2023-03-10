6ae55e283344b7ef7dc3a763070644c2ebbed9d3
thiseaskambas <tkambas@yahoo.com>
2023-03-10 12:25:39 -0400

    imported db connection in delete view

--- 02_better_version/delete.php
+++ 02_better_version/delete.php
@@ -3,8 +3,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once 'database.php';
 
 $id = $_POST['id'] ?? null;
 
